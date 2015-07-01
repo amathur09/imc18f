305e17046b0a2d22b75b209d8af927e43e4d595d
WChuang <WChuang@imc.com>
2015-07-01 07:58:02 -0400

    EIGHTEENF-4 : Passing Jenkins variables to Selenium

--- tests/automatic/runtest.bat
+++ tests/automatic/runtest.bat
@@ -1,4 +1,6 @@
 REM Run the regresstion test
+REM Parameter 1 - Jenkins Workspace
+REM Parameter 2 - Test Reports Directory
 
-cd C:\.jenkins\jobs\rxeffex\workspace\tests\automatic
-java -cp ../../lib/* com.rxeffex.test.RegressionTestDriver rxeffex.xml ../../drivers
+cd %1\tests\automatic
+java -cp %1/lib/* com.rxeffex.test.RegressionTestDriver rxeffex.xml %1/drivers %2
